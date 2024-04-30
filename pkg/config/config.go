7f3681be65409ecd6131367adf40146897f9746b
shruti2522 <shruti.apc01@gmail.com>
2024-05-01 03:34:59 +0530

    feat: add resource rules to krm-kcl spec
    Signed-off-by: shruti2522 <shruti.apc01@gmail.com>
    
    add resource matching rules to config.go
    
    Signed-off-by: shruti2522 <shruti.apc01@gmail.com>
    
    fix build and test errors
    
    Signed-off-by: shruti2522 <shruti.apc01@gmail.com>
    
    add test case for missing resource rules
    
    Signed-off-by: shruti2522 <shruti.apc01@gmail.com>
    
    updated yaml test files
    
    Signed-off-by: shruti2522 <shruti.apc01@gmail.com>
    
    test case set to optional
    
    Signed-off-by: shruti2522 <shruti.apc01@gmail.com>
    
    corrected syntax error in yaml
    
    Signed-off-by: shruti2522 <shruti.apc01@gmail.com>
    
    fixed yaml formatting in config_test.go
    
    Signed-off-by: shruti2522 <shruti.apc01@gmail.com>
    
    fixed yaml formatting
    
    Signed-off-by: shruti2522 <shruti.apc01@gmail.com>
    
    updated logic handling for MatchResourceRules
    
    Signed-off-by: shruti2522 <shruti.apc01@gmail.com>
    
    feat: add resource rules to krm-kcl spec
    
    Signed-off-by: shruti2522 <shruti.apc01@gmail.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -38,7 +38,21 @@ type KCLRun struct {
 		Source string `json:"source" yaml:"source"`
 		// Params are the parameters in key-value pairs format.
 		Params map[string]interface{} `json:"params,omitempty" yaml:"params,omitempty"`
+		// MatchConstraints defines the resource matching rules.
+		MatchConstraints MatchConstraints `json:"matchConstraints,omitempty" yaml:"matchConstraints,omitempty"`
 	} `json:"spec" yaml:"spec"`
+}
+
+// MatchConstraints defines the resource matching rules.
+type MatchConstraints struct {
+	ResourceRules []ResourceRule `json:"resourceRules,omitempty" yaml:"resourceRules,omitempty"`
+}
+
+// ResourceRule defines a rule for matching resources.
+type ResourceRule struct {
+	APIGroups   []string `json:"apiGroups,omitempty" yaml:"apiGroups,omitempty"`
+	APIVersions []string `json:"apiVersions,omitempty" yaml:"apiVersions,omitempty"`
+	Resources   []string `json:"resources,omitempty" yaml:"resources,omitempty"`
 }
 
 // Config is used to configure the KCLRun instance based on the given FunctionConfig.
@@ -123,12 +137,35 @@ func (r *KCLRun) Transform(rl *fn.ResourceList) error {
 		if err != nil {
 			return err
 		}
-		transformedObjects = append(transformedObjects, obj)
+
+		// Check if the transformed object matches the resource rules
+		if r.MatchResourceRules(obj) {
+			transformedObjects = append(transformedObjects, obj)
+		}
 	}
 	rl.Items = transformedObjects
 	return nil
 }
 
+// MatchResourceRules checks if the given Kubernetes object matches the resource rules specified in KCLRun.
+func (r *KCLRun) MatchResourceRules(obj *fn.KubeObject) bool {
+	// if MatchConstraints.ResourceRules is not set (nil or empty), return true by default
+	if r.Spec.MatchConstraints.ResourceRules == nil || len(r.Spec.MatchConstraints.ResourceRules) == 0 {
+		return true
+	}
+    // iterate through each resource rule
+	for _, rule := range r.Spec.MatchConstraints.ResourceRules {
+		if containsString(rule.APIGroups, obj.GroupKind().Group) &&
+			containsString(rule.APIVersions, obj.GetAPIVersion()) &&
+			containsString(rule.Resources, obj.GetKind()) {
+			return true
+		}
+	}
+	// if no match is found, return false
+	return false
+}
+
+// DealAnnotations handles annotations, e.g., allow-insecure-source.
 func (r *KCLRun) DealAnnotations() {
 	// Deal the allow-insecure-source annotation
 	if v, ok := r.ObjectMeta.Annotations[AnnotationAllowInSecureSource]; ok && isOk(v) {
@@ -136,10 +173,21 @@ func (r *KCLRun) DealAnnotations() {
 	}
 }
 
+// isOk checks if a given string is in the list of "OK" values.
 func isOk(value string) bool {
 	okValues := []string{"ok", "yes", "true", "1", "on"}
 	for _, v := range okValues {
 		if strings.EqualFold(strings.ToLower(value), strings.ToLower(v)) {
+			return true
+		}
+	}
+	return false
+}
+
+// containsString checks if a slice contains a string.
+func containsString(slice []string, str string) bool {
+	for _, s := range slice {
+		if s == str {
 			return true
 		}
 	}
