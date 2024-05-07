58ffbdb99046e47c0b433d60b495809ecdfdc459
peefy <xpf6677@163.com>
2024-05-07 20:14:39 +0800

    refactor: source field to impl better local package running
    Signed-off-by: peefy <xpf6677@163.com>

--- pkg/source/oci.go
+++ pkg/source/oci.go
@@ -8,22 +8,9 @@ import (
 const (
 	// OCIScheme is the URL scheme for OCI-based requests
 	OCIScheme = "oci"
-	// TarPattern is the wildcard pattern for tar files.
-	TarPattern = "*.tar"
 )
 
 // IsOCI determines whether or not a URL is to be treated as an OCI URL
 func IsOCI(src string) bool {
 	return strings.HasPrefix(src, fmt.Sprintf("%s://", OCIScheme))
-}
-
-// ReadFromOCISource reads source code from an OCI (Open Container Initiative) source.
-//
-// Parameters:
-// - src: a string containing the OCI source URL.
-//
-// Return:
-// A string containing the source code, and an error if any.
-func ReadFromOCISource(src string) (string, error) {
-	return src, nil
 }
